74cc0d8ee77825c01ccfecf383da37e00e673d80
Dhruba Jyoti Chakraborty <61685945+codewdhruv@users.noreply.github.com>
2022-08-21 01:21:05 +0530

    Update javaHelloWorldServer.java

--- javaHelloWorldServer.java
+++ javaHelloWorldServer.java
@@ -9,7 +9,7 @@ public class MyServer {
     }
 
     public static void connectToServer() {
-        try(ServerSocket serverSocket = new ServerSocket(9991)) {
+        try(ServerSocket serverSocket = new ServerSocket(8080)) {
             Socket connectionSocket = serverSocket.accept();
 
             InputStream inputToServer = connectionSocket.getInputStream();
@@ -20,13 +20,11 @@ public class MyServer {
 
             serverPrintOut.println("Hello World! Enter Peace to exit.");
 
-            //Have the server take input from the client and echo it back
-            //This should be placed in a loop that listens for a terminator text e.g. bye
             boolean done = false;
 
             while(!done && scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
+                serverPrintOut.println("Echo from Sample Server: " + line);
 
                 if(line.toLowerCase().trim().equals("peace")) {
                     done = true;
