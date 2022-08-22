db528d22060816f5aaf5a1917c1306c7873ca159
codewdhruv <dhruba.chakraborty2019@gift.edu.in>
2022-08-22 12:36:02 +0530

    docker file modified

--- javaHelloWorldServer.java
+++ javaHelloWorldServer.java
@@ -1,46 +1,6 @@
-<<<<<<< HEAD
-// File updated
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-=======
-import java.io.*;
->>>>>>> 74cc0d8ee77825c01ccfecf383da37e00e673d80
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-
-public class MyServer {
-    public static void main(String[] args) {
-        connectToServer();
-    }
-
-    public static void connectToServer() {
-        try(ServerSocket serverSocket = new ServerSocket(8080)) {
-            Socket connectionSocket = serverSocket.accept();
-
-            InputStream inputToServer = connectionSocket.getInputStream();
-            OutputStream outputFromServer = connectionSocket.getOutputStream();
-
-            Scanner scanner = new Scanner(inputToServer, "UTF-8");
-            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
-
-            serverPrintOut.println("Hello World! Enter Peace to exit.");
-
-            boolean done = false;
-
-            while(!done && scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                serverPrintOut.println("Echo from Sample Server: " + line);
-
-                if(line.toLowerCase().trim().equals("peace")) {
-                    done = true;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class Main
+{
+     public static void main(String[] args) {
+        System.out.println("Hello, World");
     }
 }
